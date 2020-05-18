50d9c3eac6ecea17d958041ae05bf02066cb340c
ronildo fernandes <beatrizsartorio@outlook.com>
2020-05-18 12:52:43 -0300

    login atualizado

--- src/Components/form-group.js
+++ src/Components/form-group.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FormGroup(props){
     return(
         <div className="form-group">                       
-            <label htmlfor={props.htmlfor}>{props.label}</label>
+            <label htmlFor={props.htmlFor}>{props.label}</label>
             {props.children}
         </div>
     )
--- src/views/login.js
+++ src/views/login.js
@@ -2,7 +2,18 @@ import React from 'react'
 import Card from '../Components/Card'
 import FormGroup from '../Components/form-group'
 
+
 class Login extends React.Component{
+   state= {
+      email:'',
+      senha:'' 
+   }
+   entrar = ()=>{
+       console.log('Email: ',this.state.email)
+       console.log('Email: ',this.state.senha) 
+
+   }
+   
     render(){
         
         return(
@@ -15,17 +26,23 @@ class Login extends React.Component{
                                 <div className="col-lg-12">
                                     <div className="bs-component">
                                         <fieldset>
-                                            <FormGroup label="Email *" htmlfor="exampleInputEmail1">
-                                            <input type="email" className="form-control" 
+                                            <FormGroup label="Email *" htmlFor="exampleInputEmail1">
+                                            <input type="email" 
+                                            value={this.state.email}
+                                            onChange={e =>this.setState({email:e.target.value})}
+                                            className="form-control" 
                                             id="exampleInputEmail1" aria-describedby="emailHelp" 
                                             placeholder="Digite o Email"/>
                                             </FormGroup>
-                                            <FormGroup label="senha *" htmlfor="exampleInputPassword1">
-                                            <input type="password" className="form-control" 
+                                            <FormGroup label="senha *" htmlFor="exampleInputPassword1">
+                                            <input type="password" 
+                                            value={this.state.senha}
+                                            onChange={e =>this.setState({senha:e.target.value})}
+                                            className="form-control" 
                                             id="exampleInputPassword1" placeholder="Password"/>
 
                                             </FormGroup>
-                                            <p><button className="btn btn-success">Entrar </button>&nbsp;&nbsp;
+                                            <p><button className="btn btn-success"onClick={this.entrar}>Entrar </button>&nbsp;&nbsp;
                                             <button className="btn btn-danger">Cadastrar</button></p>
                                         </fieldset>
                                     </div>
@@ -40,6 +57,7 @@ class Login extends React.Component{
         </div>
         )
     }
+
 }
 
 export default Login
